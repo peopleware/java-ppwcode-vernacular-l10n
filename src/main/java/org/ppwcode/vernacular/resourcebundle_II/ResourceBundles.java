f4e4ba53a2df2055c044b1820827a6486023a5e8
Jan Dockx <jan_dockx@peopleware.be>
2008-09-29 16:21:16 +0000

    make stuff compile in new structure
    git-svn-id: file:///repositories/PPWCode/java/vernacular/resourcebundle/trunk@2727 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/resourcebundle_II/ResourceBundles.java
+++ src/main/java/org/ppwcode/vernacular/resourcebundle_II/ResourceBundles.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 </license>*/
 
-package org.ppwcode.util.resourcebundle;
+package org.ppwcode.vernacular.resourcebundle_II;
 
 
 import java.util.LinkedList;
