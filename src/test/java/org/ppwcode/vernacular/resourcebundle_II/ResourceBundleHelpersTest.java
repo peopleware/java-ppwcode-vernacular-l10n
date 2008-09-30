8d216608ebde017eb929045e939531a25d280073
Jan Dockx <jan_dockx@peopleware.be>
2008-09-30 19:23:11 +0000

    new test and renamed package to resourcebundle_II
    git-svn-id: file:///repositories/PPWCode/java/vernacular/resourcebundle/trunk@2768 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ src/test/java/org/ppwcode/vernacular/resourcebundle_II/ResourceBundleHelpersTest.java
@@ -0,0 +1,48 @@
+/*<license>
+  Copyright 2008, PeopleWare n.v.
+  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
+  TO SELECTED PARTIES.
+</license>*/
+
+package org.ppwcode.vernacular.resourcebundle_II;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class ResourceBundleHelpersTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void testTypeResourceBundle() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testValueClassOfQStringArrayClassOf_T_ResourceBundleLoadStrategy() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testValueResourceBundleStringArrayClassOf_T_() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testValueResourceBundleStringClassOf_T_() {
+    fail("Not yet implemented");
+  }
+
+}
+
--- src/test/java/org/ppwcode/vernacular/resourcebundle_II/ResourceBundleLoadStrategyContract.java
+++ src/test/java/org/ppwcode/vernacular/resourcebundle_II/ResourceBundleLoadStrategyContract.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 </license>*/
 
-package org.ppwcode.vernacular.resourcebundle;
+package org.ppwcode.vernacular.resourcebundle_II;
 
 
 import static org.junit.Assert.assertTrue;
