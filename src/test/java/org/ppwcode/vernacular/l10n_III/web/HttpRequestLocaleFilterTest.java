60044ead0f1b18d2cc7989062bd959ea9f932d97
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2009-02-02 23:16:50 +0000

    adding unit tests for HttpRequestLocaleFilter, using mock objects from Spring
    git-svn-id: file:///repositories/PPWCode/java/vernacular/l10n/trunk@4074 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ src/test/java/org/ppwcode/vernacular/l10n_III/web/HttpRequestLocaleFilterTest.java
@@ -0,0 +1,115 @@
+/*<license>
+Copyright 2009 - $Date: 2008-10-15 18:22:22 +0200 (Wed, 15 Oct 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.ppwcode.vernacular.l10n_III.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import javax.servlet.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.ppwcode.vernacular.l10n_III.LocaleHelpers;
+import org.ppwcode.vernacular.l10n_III.LocaleManager;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class HttpRequestLocaleFilterTest {
+
+  private final static List<Locale> EMPTY_LOCALE_LIST = new ArrayList<Locale>();
+  private final static String PKG_NAME = "org.ppwcode.test.HttpRequestLocaleFilter";
+
+  private final static String[] SUPPORTED_LOCALES_1 = { "nl", "fr", "en" };
+  private final static String[] SUPPORTED_LOCALES_2 = { "nl", "fr-be", "fr-fr", "fr" };
+  private final static String[] PREFERRED_LOCALES_1 = { "nl-be" };
+  private final static String[] PREFERRED_LOCALES_2 = { "en", "fr-fr" };
+
+  @Before
+  public void setUp() {
+
+  }
+
+  @After
+  public void tearDown() {
+    LocaleManager.registerSupportedLocales(PKG_NAME, EMPTY_LOCALE_LIST);
+  }
+
+  @Test
+  public void testLocaleFilter1() throws IOException, ServletException {
+    testLocaleFilterHelper(SUPPORTED_LOCALES_1, PREFERRED_LOCALES_1, "nl", null);
+  }
+
+  @Test
+  public void testLocaleFilter2() throws IOException, ServletException {
+    testLocaleFilterHelper(SUPPORTED_LOCALES_1, PREFERRED_LOCALES_1, "nl", "fr-be");
+  }
+
+  @Test
+  public void testLocaleFilter3() throws IOException, ServletException {
+    testLocaleFilterHelper(SUPPORTED_LOCALES_2, PREFERRED_LOCALES_2, "fr-fr", null);
+  }
+
+  @Test
+  public void testLocaleFilter4() throws IOException, ServletException {
+    testLocaleFilterHelper(SUPPORTED_LOCALES_2, PREFERRED_LOCALES_2, "fr-fr", "fr");
+  }
+
+  @Test
+  public void testLocaleFilter5() throws IOException, ServletException {
+    testLocaleFilterHelper(SUPPORTED_LOCALES_2, PREFERRED_LOCALES_2, "fr-fr", "nl-be");
+  }
+
+
+  
+  private void testLocaleFilterHelper(String[] supportedLocales, String[] preferredLocales, String bestLocale, String sessionLocale)
+          throws IOException, ServletException {
+
+    MockHttpServletRequest request = new MockHttpServletRequest("GET", "/index.html");
+
+    for (int i = preferredLocales.length -1; i >= 0; i--) {
+      request.addPreferredLocale(LocaleHelpers.constructLocaleFromString(preferredLocales[i]));
+    }
+
+    MockHttpServletResponse response = new MockHttpServletResponse();
+    MockFilterChain chain = new MockFilterChain();
+    HttpSession session = request.getSession();
+
+    if (sessionLocale != null) {
+      session.setAttribute(HttpRequestLocaleFilter.ATTRIBUTE_PREFERRED_LOCALE, 
+              LocaleHelpers.constructLocaleFromString(sessionLocale));
+    }
+
+    LocaleManager.registerSupportedLocales(PKG_NAME, LocaleHelpers.constructListOfLocales(supportedLocales));
+
+    Filter localeFilter = new HttpRequestLocaleFilter();
+    localeFilter.doFilter(request, response, chain);
+
+    if (sessionLocale == null) {
+      Assert.assertTrue(session.getAttribute(HttpRequestLocaleFilter.ATTRIBUTE_PREFERRED_LOCALE).
+            equals(LocaleHelpers.constructLocaleFromString(bestLocale)));
+    } else {
+      Assert.assertTrue(session.getAttribute(HttpRequestLocaleFilter.ATTRIBUTE_PREFERRED_LOCALE).
+            equals(LocaleHelpers.constructLocaleFromString(sessionLocale)));
+    }
+  }
+
+}
